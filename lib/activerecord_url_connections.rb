bad52ed7df79035a23ec87e3efcebb4820cf85c2
Glenn Gillen <me@glenngillen.com>
2011-06-28 14:43:14 +0100

    Moving URL parsing back into separate class where it should have been.

--- lib/activerecord_url_connections.rb
+++ lib/activerecord_url_connections.rb
@@ -2,14 +2,23 @@ require "active_record"
 module ActiveRecordURLConnections
   def self.parse(str)
     config = URI.parse(str)
-    { :encoding => "unicode",
-      :port     => config.port,
-      :username => config.user,
-      :adapter  => "postgresql",
-      :database => config.path.gsub(%r{/},""),
-      :host     => config.host,
-      :password => config.password
-    }
+    adapter = config.scheme
+    adapter = "postgresql" if adapter == "postgres"
+    spec = { :adapter  => adapter,
+             :username => config.user,
+             :password => config.password,
+             :port     => config.port,
+             :database => config.path.sub(%r{^/},""),
+             :hostname => config.host }
+    spec.reject!{ |key,value| value.nil? }
+    spec.merge!(split_query_options(config.query))
+    spec
+  end
+
+  private
+  def self.split_query_options(query = nil)
+    return {} unless query
+    Hash[query.split("&").map{ |pair| pair.split("=") }].symbolize_keys
   end
 end
 
@@ -19,26 +28,11 @@ ActiveRecord::Base.class_eval do
     def establish_connection(spec = nil)
       spec ||= ENV["DATABASE_URL"]
       if spec.is_a?(String) && url = URI.parse(spec)
-        adapter = url.scheme
-        adapter = "postgresql" if adapter == "postgres"
-        spec = { :adapter  => adapter,
-                 :username => url.user,
-                 :password => url.password,
-                 :database => url.path.sub(%r{^/},""),
-                 :port     => url.port,
-                 :hostname => url.host }
-        spec.reject!{ |key,value| value.nil? }
-        spec.merge!(split_query_options(url.query))
+        spec = ActiveRecordURLConnections.parse(spec)
       end
       establish_connection_without_url(spec)
     rescue URI::InvalidURIError
       establish_connection_without_url(spec)
-    end
-
-    private
-    def split_query_options(query = nil)
-      return {} unless query
-      Hash[query.split("&").map{ |pair| pair.split("=") }].symbolize_keys
     end
   end
 end
